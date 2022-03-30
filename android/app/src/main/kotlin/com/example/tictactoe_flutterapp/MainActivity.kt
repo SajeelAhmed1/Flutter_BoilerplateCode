c704753b4d05141f34aee233bc72c80759ca7b72
SajeelAhmed1 <sajeelahmedsiddiqui@gmail.com>
2022-03-30 17:35:42 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/tictactoe_flutterapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tictactoe_flutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+// import 'package:my_first_flutter_app/app_button.dart';
+// import 'package:my_first_flutter_app/app_textField.dart';
+import 'package:tictactoe_flutterapp/screens/homepage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    // MaterialApp app=MaterialApp()
+    // return app()
+    return MaterialApp(
+      home: SignUp(),
+      debugShowCheckedModeBanner: false ,
+    );
+  }
+}
+
+ // class HomeScreen extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title:  ElevatedButton(
+//             onPressed: (){
+//               print("Button Has been clicked");}
+//             , child: const Text('Button')),
+//       ),
+//     body: const Center(
+//       child: Text("Hello App",
+//           style:TextStyle(fontSize:80, color:Colors.deepPurple )),
+//     )
+//     );
+//   }
+// }
+
+
+
+
+
+
+
+
